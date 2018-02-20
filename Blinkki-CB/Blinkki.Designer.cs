598c3e83bec0765844f84d7b0ed93ad409c1552a
Mika Leiman <mika.leiman@outlook.com>
2018-02-20 19:31:58 +0200

    Initial commit with nupkg configs for CefSharp.Common.63.0.1/CefSharp.Common.63.0.1.nupkg  and DockPanelSuite.3.0.4.nupkg

---
+++ Blinkki-CB/Blinkki.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace Blinkki_CB
+{
+    partial class Blinkki
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Blinkki));
+            this.SuspendLayout();
+            // 
+            // Blinkki
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 601);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Blinkki";
+            this.Text = "Blinkki - Code Book";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
