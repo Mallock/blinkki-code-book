f8fd02c883995363263a7981f64084c3be126bf7
Mika Leiman <mika.leiman@gmail.com>
2021-02-06 16:12:35 +0200

    Added browser user agent workaround for google login Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0

--- Blinkki-CB/Blinkki.cs
+++ Blinkki-CB/Blinkki.cs
@@ -117,6 +117,8 @@ namespace Blinkki_CB
 
             //settings.CefCommandLineArgs.Add("enable-npapi", "1");
             settings.Locale = CultureInfo.CurrentCulture.Name;
+            settings.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0";
+
             Cef.Initialize(settings);
             OpenNewTab("https://code.blinkki.com/");
             LoadFavouriteTools();
